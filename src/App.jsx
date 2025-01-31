868af4b42fde46dbccbd039b3be898f5399721ae
Poovanna <poovannakeethianda@gmail.com>
2025-01-31 14:50:00 +0530

    Filtering logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,70 @@ import "./index.css";
 import Navigation from "./Navigation/Nav.jsx";
 import Products from "./Products/Products.jsx";
 import Recommended from "./Recommended/Recommended.jsx";
-import Category from "./Sidebar/Category/Category.jsx";
-import Colors from "./Sidebar/Colors/Colors.jsx";
-import Price from "./Sidebar/Price/Price.jsx";
-import Size from "./Sidebar/Size/Size.jsx";
 import Sidebar from "./Sidebar/Sidebar.jsx";
+import Card from "./components/Card.jsx";
+
+//data
+import productData from "./db/data.jsx";
 
 const App = () => {
+  const [selectedCategory, setSelectedCategory] = useState(null);
+  const [query, setQuery] = useState("");
+
+  //-----------input filter------------------
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const filteredItems = productData.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
+
+  //-------------radio filter------------
+  const handleRadioChange = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  //-------------buttons filter------------
+  const handleButtonClick = (e) => {
+    setSelectedCategory(e.target.value);
+  };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    //filtering input items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    //selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, newPrice, prevPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
   return (
     <>
       <Sidebar />
--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./Colors.css";
+import Input from "../../components/Input";
 
 const Colors = () => {
-  return <div>Colors</div>;
+  return (
+    <div>
+      <h2 className="sidebar-title colors-title">Colors</h2>
+      <Input />
+    </div>
+  );
 };
 
 export default Colors;
